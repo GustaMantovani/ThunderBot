b93bac8498a4efd537387482c425b7389bd7706d
Gustavo Mantovani <mantovanig000@gmail.com>
2024-05-13 20:06:25 -0300

    adding a brief description about the bot in the help message and authorship information at the end with my photo

--- bot.py
+++ bot.py
@@ -285,62 +285,72 @@ async def prices(ctx,lang,cardName):
     
 @bot.event
 async def on_message(message):
-    #Check if the message was sent by the bot to avoid loops
+    # Check if the message was sent by the bot to avoid loops
     if message.author == bot.user:
         return
 
-    #Check if the message is specific
+    # Check if the message is specific
     if message.content == 'ThunderBot ajuda':
-        #Embed message for Portuguese (Brazil)
-        embed_pt = discord.Embed(title="Comandos disponíveis", description="Lista de comandos disponíveis para usar com o bot:", color=0x7289da)
+        # Embed message for Portuguese (Brazil)
+        embed_pt = discord.Embed(title="Sobre o ThunderBot", description="ThunderBot é projetado para fornecer informações detalhadas sobre cartas de Yu-Gi-Oh!", color=0x7289da)
+        embed_pt.add_field(name="Comandos disponíveis", value="Lista de comandos disponíveis para usar com o bot:", inline=False)
         embed_pt.add_field(name="!card (lang) (cardName)", value="Retorna a imagem da carta especificada.", inline=False)
         embed_pt.add_field(name="!info (lang) (cardName)", value="Fornece informações detalhadas sobre a carta especificada.", inline=False)
         embed_pt.add_field(name="!tr (original_lang) (cardName) (translate_lang)", value="Traduz o nome da carta de um idioma para outro.", inline=False)
         embed_pt.add_field(name="!search (lang) (cardName)", value="Pesquisa cartas com base em uma sequência de caracteres.", inline=False)
-        embed_pt.add_field(name="!prices (lang) (cardName)", value="Mostra os preços da carta especificada.", inline=False)  
+        embed_pt.add_field(name="!prices (lang) (cardName)", value="Mostra os preços da carta especificada.", inline=False)
+        embed_pt.set_footer(text="Criado por Gustavo Mantovani", icon_url="https://avatars.githubusercontent.com/u/112289861?v=4")  
         await message.channel.send(embed=embed_pt)
 
     elif message.content == 'ThunderBot help':
-        #Embed message for English
-        embed_en = discord.Embed(title="Available Commands", description="List of available commands to use with the bot:", color=0x7289da)
+        # Embed message for English
+        embed_en = discord.Embed(title="About ThunderBot", description="ThunderBot is designed to provide detailed information about Yu-Gi-Oh! cards.", color=0x7289da)
+        embed_en.add_field(name="Available Commands", value="List of available commands to use with the bot:", inline=False)
         embed_en.add_field(name="!card (lang) (cardName)", value="Returns the image of the specified card.", inline=False)
         embed_en.add_field(name="!info (lang) (cardName)", value="Provides detailed information about the specified card.", inline=False)
         embed_en.add_field(name="!tr (original_lang) (cardName) (translate_lang)", value="Translates the name of the card from one language to another.", inline=False)
         embed_en.add_field(name="!search (lang) (cardName)", value="Searches cards based on a character sequence.", inline=False)
-        embed_en.add_field(name="!prices (lang) (cardName)", value="Shows the prices of the specified card.", inline=False) 
+        embed_en.add_field(name="!prices (lang) (cardName)", value="Shows the prices of the specified card.", inline=False)
+        embed_en.set_footer(text="Created by Gustavo Mantovani", icon_url="https://avatars.githubusercontent.com/u/112289861?v=4")  
         await message.channel.send(embed=embed_en)
 
     elif message.content == 'ThunderBot hilfe':
-        #Embed message for German
-        embed_de = discord.Embed(title="Verfügbare Befehle", description="Liste der verfügbaren Befehle, die mit dem Bot verwendet werden können:", color=0x7289da)
+        # Embed message for German
+        embed_de = discord.Embed(title="Über ThunderBot", description="ThunderBot wurde entwickelt, um detaillierte Informationen über Yu-Gi-Oh!-Karten bereitzustellen.", color=0x7289da)
+        embed_de.add_field(name="Verfügbare Befehle", value="Liste der verfügbaren Befehle, die mit dem Bot verwendet werden können:", inline=False)
         embed_de.add_field(name="!card (lang) (Kartenname)", value="Gibt das Bild der angegebenen Karte zurück.", inline=False)
         embed_de.add_field(name="!info (lang) (Kartenname)", value="Gibt detaillierte Informationen zur angegebenen Karte zurück.", inline=False)
         embed_de.add_field(name="!tr (original_lang) (Kartenname) (translate_lang)", value="Übersetzt den Namen der Karte von einer Sprache in eine andere.", inline=False)
         embed_de.add_field(name="!search (lang) (Kartenname)", value="Sucht nach Karten anhand einer Zeichenfolge.", inline=False)
-        embed_de.add_field(name="!prices (lang) (Kartenname)", value="Zeigt die Preise der angegebenen Karte an.", inline=False)  
+        embed_de.add_field(name="!prices (lang) (Kartenname)", value="Zeigt die Preise der angegebenen Karte an.", inline=False)
+        embed_de.set_footer(text="Erstellt von Gustavo Mantovani", icon_url="https://avatars.githubusercontent.com/u/112289861?v=4")  
         await message.channel.send(embed=embed_de)
 
     elif message.content == 'ThunderBot aiuto':
-        #Embed message for Italian
-        embed_it = discord.Embed(title="Comandi disponibili", description="Elenco dei comandi disponibili da utilizzare con il bot:", color=0x7289da)
+        # Embed message for Italian
+        embed_it = discord.Embed(title="Su ThunderBot", description="ThunderBot è progettato per fornire informazioni dettagliate sulle carte Yu-Gi-Oh!", color=0x7289da)
+        embed_it.add_field(name="Comandi disponibili", value="Elenco dei comandi disponibili da utilizzare con il bot:", inline=False)
         embed_it.add_field(name="!card (lang) (nomeCarta)", value="Restituisce l'immagine della carta specificata.", inline=False)
         embed_it.add_field(name="!info (lang) (nomeCarta)", value="Fornisce informazioni dettagliate sulla carta specificata.", inline=False)
         embed_it.add_field(name="!tr (original_lang) (nomeCarta) (translate_lang)", value="Traduce il nome della carta da una lingua all'altra.", inline=False)
         embed_it.add_field(name="!search (lang) (nomeCarta)", value="Cerca carte basate su una sequenza di caratteri.", inline=False)
-        embed_it.add_field(name="!prices (lang) (nomeCarta)", value="Mostra i prezzi della carta specificata.", inline=False)  
+        embed_it.add_field(name="!prices (lang) (nomeCarta)", value="Mostra i prezzi della carta specificata.", inline=False)
+        embed_it.set_footer(text="Creato da Gustavo Mantovani", icon_url="https://avatars.githubusercontent.com/u/112289861?v=4")  
         await message.channel.send(embed=embed_it)
 
     elif message.content == 'ThunderBot aide':
-        #Embed message for French
-        embed_fr = discord.Embed(title="Commandes disponibles", description="Liste des commandes disponibles à utiliser avec le bot:", color=0x7289da)
+        # Embed message for French
+        embed_fr = discord.Embed(title="À propos de ThunderBot", description="ThunderBot est conçu pour fournir des informations détaillées sur les cartes Yu-Gi-Oh!", color=0x7289da)
+        embed_fr.add_field(name="Commandes disponibles", value="Liste des commandes disponibles à utiliser avec le bot:", inline=False)
         embed_fr.add_field(name="!card (lang) (nomCarte)", value="Renvoie l'image de la carte spécifiée.", inline=False)
         embed_fr.add_field(name="!info (lang) (nomCarte)", value="Fournit des informations détaillées sur la carte spécifiée.", inline=False)
         embed_fr.add_field(name="!tr (original_lang) (nomCarte) (translate_lang)", value="Traduit le nom de la carte d'une langue à une autre.", inline=False)
         embed_fr.add_field(name="!search (lang) (nomCarte)", value="Recherche des cartes basées sur une séquence de caractères.", inline=False)
-        embed_fr.add_field(name="!prices (lang) (nomCarte)", value="Affiche les prix de la carte spécifiée.", inline=False)  
+        embed_fr.add_field(name="!prices (lang) (nomCarte)", value="Affiche les prix de la carte spécifiée.", inline=False)
+        embed_fr.set_footer(text="Créé par Gustavo Mantovani", icon_url="https://avatars.githubusercontent.com/u/112289861?v=4")  
         await message.channel.send(embed=embed_fr)
 
-    #Call the default command processing after the on_message event
+    # Call the default command processing after the on_message event
     await bot.process_commands(message)
 #
     
